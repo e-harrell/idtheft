d6e0f7e7f1367c87d92a9a13834ec6bb4bc16f6b
karieslexia <E_A_Harrell@hotmail.com>
2021-02-17 23:07:13 -0500

    updated script with added machine learning code

--- script.R
+++ script.R
@@ -386,4 +386,45 @@ chisq.test(its1$idtheft,its1$prevent_total)
 chisq.test(its1$idtheft,its1$OUTSIDE_PAST_YEARR)
 chisq.test(its1$idtheft,its1$notify_breachr)
 
+#machine learning stuff
+
+#change reference groups in variables
+its1$ager<-relevel(its1$ager, ref = 4)
+its1$ethnicr<-relevel(its1$ethnicr, ref = 2)
+its1$notify_breachr<-relevel(its1$notify_breachr, ref =2)
+its1$prevent_total<-relevel(its1$prevent_total, ref=2)
+
+inTrain<-createDataPartition(its1$idtheft,p=.6, list=FALSE)
+#put 60% in training (inTrain)
+training <- its1[inTrain,]
+#put 40% in test(not inTrain)
+testing <- its1[-inTrain,]
+
+#check for predictors with no variability in training data
+nzv<-nearZeroVar(training, saveMetrics = TRUE)
+nzv
+
+#train logistic regression model using all predictors except sex
+modFit <- glm(idtheft ~ incomer+ager+ethnicr+prevent_total+OUTSIDE_PAST_YEARR+notify_breachr
+              ,data=training,family="binomial")
+#get model estimates
+summary(modFit)
+#Odds ratio
+exp(modFit$coeff)
+#get variance inflation factors
+vif(modFit)
+#anova- used for putting factors in and out of model
+anova(modFit, test = "Chisq")
+
+#adjusted model without preventative measures
+modFit1 <- glm(idtheft ~ incomer+ager+ethnicr+OUTSIDE_PAST_YEARR+notify_breachr
+              ,data=training,family="binomial")
+#get model estimates
+summary(modFit1)
+#Odds ratio
+exp(modFit1$coeff)
+#get variance inflation factors
+vif(modFit1)
+#anova- used for putting factors in and out of model
+anova(modFit1, test = "Chisq")
 
